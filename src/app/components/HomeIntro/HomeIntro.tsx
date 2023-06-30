8f26786708167fe4fb2ed017f1b244400abeb937
Marcelo Sousa <mns.marcelo67@gmail.com>
2023-06-30 08:59:27 -0300

    add media query home intro

--- src/app/components/HomeIntro/HomeIntro.tsx
+++ src/app/components/HomeIntro/HomeIntro.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FaGithubSquare, FaLinkedin, FaInstagramSquare } from 'react-icons/fa';
+
 import styles from './homeintro.module.css'
 
 export const HomeIntro: React.FC = () => {
@@ -8,17 +10,41 @@ export const HomeIntro: React.FC = () => {
             <div className={styles.homeIntroWrapper}>
                 <div className={styles.headerHero}>
                     <span>
-                        I'm a front-end
+                        {`I'm a front-end`}
                     </span>
                     <br />
                     <span>
-                        developer
+                        developer.
                     </span>
                 </div>
                 <div className={styles.buttonCVWrapper}>
                     <button className={styles.button64} role="button">
                         <span className="text">Download CV</span>
                     </button>
+                </div>
+                <div className={styles.socialLinks}>
+                    <a href="https://github.com/mnsmarcelo"
+                       target="_blank"
+                       rel="noreferrer"
+                       title="GitHub profile"
+                    >
+                        <FaGithubSquare />
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/mnsmarcelo/"
+                       target="_blank"
+                       rel="noreferrer"
+                       title="Linkedin profile"
+                    >
+                    <FaLinkedin />
+                    </a>
+                    <a href="https://www.instagram.com/mnsmarcelo"
+                       target="_blank"
+                       rel="noreferrer"
+                       title="Instagram profile"
+                    >
+                        <FaInstagramSquare />
+                    </a>
                 </div>
             </div>
         </div>
