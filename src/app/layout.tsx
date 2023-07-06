72bdc50e94d780d0237a40f63a5dec0515e8dc45
Marcelo Sousa <mns.marcelo67@gmail.com>
2023-07-05 22:16:47 -0300

    add og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,17 @@ import { Quicksand } from 'next/font/google';
 const quicksand = Quicksand({ subsets: ['latin'] });
 
 export const metadata = {
+  metadataBase: new URL('https://marcelosousa.tech'),
   title: 'Marcelo Sousa | Web Developer',
   description: 'Web Developer',
   themeColor: '#13131f',
+  openGraph: {
+    title: 'Marcelo Sousa',
+    description: 'I build things for the web',
+    type: 'website',
+    url: 'https://marcelosousa.tech',
+    images: 'image-share-site.jpg',
+  },
 };
 
 export default function RootLayout({
